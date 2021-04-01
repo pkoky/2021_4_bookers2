2ad6f030ba3aaa8004eb5e9a58eef704c1ce6e61
pkoky <sscsk2224@gmail.com>
2021-04-01 11:24:55 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @booknew = Book.new
   end
 
   def index
@@ -23,6 +24,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if
+      @book.user == current_user
+    else
+      redirect_to books_path
+    end
+
   end
 
 
@@ -36,9 +43,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
